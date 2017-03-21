0a67f1078cf682924b03f60ab02f4564f20ad4a5
U-AMAYBERRY-LAPTO\voyager <hollywoodno@gmail.com>
2017-03-21 08:58:34 -0700

    add templates for login and logging interface

---
+++ app.py
@@ -0,0 +1,77 @@
+# Python libs
+
+# 3rd party libs
+from flask import Flask, render_template, request, redirect, flash, url_for
+from flask_login import LoginManager, login_required, login_user, logout_user
+
+# My libs
+import models
+
+
+app = Flask(__name__, static_url_path="/static")
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'oribooboo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///logbud'
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return models.UserProfile.query.get(user_id)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = models.LoggerLoginForm()
+    if form.validate_on_submit():
+        user = models.UserProfile.query.filter_by(email=form.email.data).first()
+
+        # Login and validate the user.
+        if user:
+            if user.password == form.password.data:
+                user.authenticated = True
+                user.active = True
+                login_user(user, remember=False)
+
+                flash('Logged in successfully.')
+
+                next = request.args.get('next')
+                # TODO:
+                # is_safe_url should check if the url is safe for redirects.
+                # See http://flask.pocoo.org/snippets/62/ for an example.
+                # if not flask_login.is_safe_url(next):
+                #     return app.abort(400)
+                return redirect(next or url_for('.logbud'))
+
+        # TODO:
+            # Set up registering new users
+
+        flash("Invalid credentials!")
+    return render_template('login.html', form=form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out successfully.')
+    return redirect('/')
+
+
+@app.route('/logbud')
+@login_required
+def logbud():
+
+    return render_template('logbud.html')
+
+if __name__ == "__main__":
+    models.connect_to_db(app)
+    app.run()
---
+++ models.py
@@ -0,0 +1,40 @@
+# Python libs
+
+# 3rd party libs
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import StringField, PasswordField, validators, SubmitField
+from flask_wtf import Form
+from flask_login import UserMixin
+
+# My libs
+from app import app
+
+
+db = SQLAlchemy(app)
+
+
+class LoggerLoginForm(Form):
+    """ Login for logger user accounts """
+
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('Password')
+    submit = SubmitField('Submit')
+
+
+class UserProfile(db.Model, UserMixin):
+    """Users model"""
+
+    __tablename__ = "user_profiles"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=True)
+    password = db.Column(db.String(100), nullable=True)
+
+
+def connect_to_db(app, db_uri=None):
+    """ Connect application to database """
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///logbud'
+    # app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+    db.create_all(app=app)
